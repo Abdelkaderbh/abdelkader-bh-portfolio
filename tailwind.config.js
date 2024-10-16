7028f0bd893a62e1889cee098e2d230b421bfcca
Abdelkaderbh <gaderbh2@gmail.com>
2024-10-16 18:22:30 +0100

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,31 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "15px",
+    },
+    screens: {
+      sm: "648px",
+      md: "768px",
+      lg: "1824px",
+      xl: "1288px",
+    },
+    fontFamily: {
+      primary: "var(--font-bebas)",
+      secondary: "var(--font-inter)",
+    },
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: "10151F",
+        secondary: "#475467",
+        accent: {
+          DEFAULT: "7edad2",
+          HOVER: "#79d3cc",
+        },
+      },
+      boxShadow: {
+        custom: "0px 14px 54 rgba(0,0,0,0.1)",
       },
     },
   },
